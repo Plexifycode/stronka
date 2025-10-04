e986007b0e944c2bb3b8699601d56857b1fb944f
Tomasz Łabędzki <tlabedzki00@gmail.com>
2025-10-04 19:56:25 +0200

    more navigator styling

--- app/components/navigator.js
+++ app/components/navigator.js
@@ -2,11 +2,23 @@
 
 import { useEffect, useState, useRef } from "react";
 
-const moveToPage = function (pageIndex) {
-    document.getElementById("app").style.left = `-${100 * pageIndex}vw`;
-};
-
 const Navigator = function () {
+    const navButtons = useRef([]);
+    const moveToPage = function (pageIndex) {
+        const clickedButton = navButtons.current[pageIndex];
+        if (clickedButton.classList.contains("nav-button-active")) {
+            return;
+        }
+        document.getElementById("app").style.left = `-${100 * pageIndex}vw`;
+        for (let i = 0; i < navButtons.current.length; i++) {
+            const iNavButton = navButtons.current[i];
+            if (iNavButton.classList.contains("nav-button-active")) {
+                iNavButton.className = "w-6 aspect-square z-5 rounded-full bg-white/40 hover:bg-white/60 hover:scale-140 hover:cursor-pointer active:bg-white/80 active:scale-120 active:blur-[1px] transition-all duration-250 active:duration-75";
+            }
+        }
+        clickedButton.className = "w-6 aspect-square z-5 rounded-full bg-green-300/40 hover:bg-green-300/60 scale-130 nav-button-active hover:scale-140 hover:cursor-pointer active:bg-green-300/80 active:scale-120 active:blur-[1px] transition-all duration-250 active:duration-75"
+    };
+
     const navElement = useRef(null);
     const slideNavUp = function () {
         if (!navElement.current.classList.contains("translate-y-3/4")) {
@@ -26,26 +38,34 @@ const Navigator = function () {
         const elementsArray = Array.from(elements);
         setAlbumPages(elementsArray);
 
-        setTimeout(slideNavDown, 1000)
+        setTimeout(() => {
+            moveToPage(0);
+        }, 200);
+
+        setTimeout(slideNavDown, 1500);
     }, []);
     return (
-        
         <div
             ref={navElement}
             className="fixed grid grid-rows-1 auto-cols-fr grid-flow-col left-1/2 -translate-x-1/2 bottom-0 w-80 h-20 px-8 place-items-center bg-white/10 backdrop-blur-md rounded-t-2xl drop-shadow-[0_0_8px] drop-shadow-black/40 transition-all duration-300 ease"
             onMouseEnter={slideNavUp}
             onMouseLeave={slideNavDown}>
             {albumPages &&
-                albumPages.map((dziecko, index) => (
+                albumPages.map((_, index) => (
                     <div
                         key={index}
+                        ref={(node) => {
+                            if (node) {
+                                navButtons.current[index] = node;
+                            }
+                        }}
                         onClick={() => {
                             moveToPage(index);
                         }}
                         className="w-6 aspect-square z-5 rounded-full bg-white/40 hover:bg-white/60 hover:scale-140 hover:cursor-pointer active:bg-white/80 active:scale-120 active:blur-[1px] transition-all duration-250 active:duration-75"
                     />
                 ))}
-            <div className="absolute -left-1/8 bottom-0 w-5/4 h-3/2 bg-red-500/0" />
+            <div className="absolute -left-1/8 bottom-0 w-5/4 h-3/2" />
         </div>
     );
 };
